5baa809e14ac607d95d9db97a9ee414d8fe4295f
OngBen <104296848+OngBen@users.noreply.github.com>
2022-11-26 20:37:13 +0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,5 +1,7 @@
 ## Again we import the necessary socket python module
 import socket
+import json
+
 ## Here we define the UDP IP address as well as the port number that we have
 ## already defined in the client python script.
 UDP_IP_ADDRESS = "127.0.0.1"
@@ -12,5 +14,12 @@ serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
 while True:
     data, addr = serverSock.recvfrom(1024)
+    
+    data = data.decode('utf-8')
+    data = data.replace("'", '"')
+    print (type(data))
     print ("Message: ", data)
+    json_obj = json.loads(data)
+    print (type(json_obj))
+    print (json_obj['message'])
     
