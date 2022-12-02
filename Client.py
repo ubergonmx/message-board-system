fa6f2f31e8e6d7fd2bbf19bef073cea6ae866a9e
OngBen <104296848+OngBen@users.noreply.github.com>
2022-12-02 18:13:07 +0800

    Update Client.py

--- Client.py
+++ Client.py
@@ -10,6 +10,7 @@ def syntaxcommands():
 
 
 import socket
+import sys
 import json
 UDP_IP_ADDRESS = "127.0.0.1"
 UDP_PORT_NO = 6789
@@ -18,30 +19,51 @@ Message = "Hello, Server"
 clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 Input = ""
-while Input != "leave":
+allowed = 0;
+
+while True:
     Input = input("Enter your command: ")
-    json = {}
-    if Input[:5] == "join ":    
-        json = {'command': 'join'}
-        print("join")
+    json_obj = {}
+    
+    if Input[:5] == "join ":        
+        string = Input.split()   
+        json_obj = {'command': 'join'}
+        json_obj = json.dumps(json_obj)
+        if (string[1] == "127.0.0.1" and int(string[2]) == 6789):
+            allowed = 1 
+            clientSock.sendto(bytes(json_obj, "utf-8"), ("127.0.0.1", 6789))
+        else:
+            print("Error: Connection to the Message Board Server has failed! Please check IP Address and Port Number.")
     elif Input == "leave":
-        json = {'command': 'leave'}
+        json_obj = {'command': 'leave'}
+        json_obj = json.dumps(json_obj)
+        clientSock.sendto(bytes(json_obj, "utf-8"), (UDP_IP_ADDRESS, UDP_PORT_NO))
         break;    
     elif Input[:9] == "register ":
-        json = {'command': 'register', 'handle': 'handle'}       
-        print("register")
+        string = Input.split()  
+        json_obj = {'command': 'register', 'handle': str(string[1])}
+        json_obj = json.dumps(json_obj)
+        clientSock.sendto(bytes(json_obj, "utf-8"), (UDP_IP_ADDRESS, UDP_PORT_NO))
     elif Input[:4] == "all ":
-        json = {"command": "all", "message": Message}
-        print(str.encode(str(json)))
-        clientSock.sendto(str.encode(str(json)), (UDP_IP_ADDRESS, UDP_PORT_NO))
+        json_obj = {"command": "all", "message": Message}
+        json_obj = json.dumps(json_obj)
+        clientSock.sendto(bytes(json_obj, "utf-8"), (UDP_IP_ADDRESS, UDP_PORT_NO))
         print("all")
     elif Input[:4] == "msg ":
-        json = {'command': 'msg', 'handle': 'handle',  'message': 'message'}
+        json_obj = {'command': 'msg', 'handle': 'handle',  'message': 'message'}
         print("message")
     elif Input == "?":
         syntaxcommands()
     else:
-        json = {'command': 'all', 'message': '<error_message>'}
-        print("Invalid Command")
-
-
+        json_obj = {'command': 'error', 'message': '<error_message>'}
+        json_obj = json.dumps(json_obj)
+        clientSock.sendto(bytes(json_obj, "utf-8"), (UDP_IP_ADDRESS, UDP_PORT_NO))
+        
+    if (allowed == 1):
+        data, server = clientSock.recvfrom(1024)  
+        if data:
+            data = data.decode('utf-8')
+            data = data.replace("'", '"')
+            data = json.loads(data)
+            print (data)
+        
